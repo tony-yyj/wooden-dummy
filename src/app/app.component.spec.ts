d52d7fc0092e4f3e5916d8c3bb020d0505f5b369
tony yang <tcsyyj@gmail.com>
2019-03-07 22:33:30 +0800

    研究单元测试

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('wooden-dummy');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a h2 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wooden-dummy!');
+    expect(compiled.querySelector('h2').textContent).toContain('Welcome to wooden-dummy!');
   });
 });
